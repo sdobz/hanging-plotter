873ad4afe21ec817797a7282c21382a7df09110d
jared-w <jaredweakly@gmail.com>
2020-03-16 20:57:42 -0700

    Reformat shell.nix with nixfmt

--- shell.nix
+++ shell.nix
@@ -1,19 +1,12 @@
-{ pkgs ? import <nixpkgs> {} }:
-let 
-  esp-idf = (pkgs.callPackage ./esp-idf.nix {});
-  esp32-toolchain = (pkgs.callPackage ./esp32-toolchain.nix {});
-in
-  pkgs.mkShell {
-    buildInputs = [ 
-      esp-idf
-      esp32-toolchain
-    ];
-    shellHook = ''
-set -e
-
-export IDF_PATH=${esp-idf}
-
-export NIX_CFLAGS_LINK=-lncurses
-export PATH=$PATH:$IDF_PATH/tools
-    '';
+{ pkgs ? import <nixpkgs> { } }:
+let
+  esp-idf = (pkgs.callPackage ./esp-idf.nix { });
+  esp32-toolchain = (pkgs.callPackage ./esp32-toolchain.nix { });
+in pkgs.mkShell {
+  buildInputs = [ esp-idf esp32-toolchain ];
+  shellHook = ''
+    export IDF_PATH=${esp-idf}
+    export NIX_CFLAGS_LINK=-lncurses
+    export PATH=$PATH:$IDF_PATH/tools
+  '';
 }
