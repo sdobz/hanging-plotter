2c542729b24a2559cd4a2855545c51a2a069eec4
sdobz <vincent@khougaz.com>
2020-05-13 13:28:17 -0700

    Tracking esp32-hello repo and switch to rust-esp-nix

--- esp32/shell.nix
+++ esp32/shell.nix
@@ -1,23 +1,4 @@
 let
-  # idf-package-overlay = self: super:
-  #   # Within the overlay we use a recursive set, though I think we can use `self` as well.
-  #   rec {
-  #     # nix-shell -p python.pkgs.my_stuff
-  #     python2 = super.python2.override {
-  #       # Careful, we're using a different self and super here!
-  #       packageOverrides = self: super: {
-  #         pyparsing = super.buildPythonPackage rec {
-  #           pname = "pyparsing";
-  #           version = "2.3.1";
-  #           doCheck = false;
-  #           src = super.fetchPypi {
-  #             inherit pname version;
-  #             sha256 = "0yk6xl885b91dmlhlsap7x78hk2rdr879fln9anbq6k4ca42djb6";
-  #           };
-  #         };
-  #       };
-  #     };
-  #   };
   pythonPackageOverrides = self: super: {
     pyparsing = super.buildPythonPackage rec {
       pname = "pyparsing";
@@ -29,109 +10,53 @@ let
       };
     };
   };
-  
- idf-package-overlay = self: super: {
-    python2 = super.python2.override {
-    packageOverrides = pythonPackageOverrides;
-    };
- };
 
-  pkgs = import <unstable> {
+  idf-package-overlay = self: super: {
+    python2 = super.python2.override {
+      packageOverrides = pythonPackageOverrides;
+    };
+  };
+
+  pkgs = import (builtins.fetchTarball {
+    # https://releases.nixos.org/nixos/unstable/nixos-20.09pre223023.fce7562cf46
+    name = "nixos-unstable-2020-04-30";
+    url = "https://github.com/nixos/nixpkgs/archive/fce7562cf46727fdaf801b232116bc9ce0512049.tar.gz";
+    sha256 = "14rvi69ji61x3z88vbn17rg5vxrnw2wbnanxb7y0qzyqrj7spapx";
+  }) {
     overlays = [
       idf-package-overlay
     ];
   };
-in let
-  esp-idf = (pkgs.callPackage ./esp-idf.nix {});
-  esp32-toolchain = (pkgs.callPackage ./esp32-toolchain-4.0.nix {});
-  llvm-xtensa = (pkgs.callPackage ./llvm-xtensa.nix {});
-  stdenv = pkgs.stdenv;
-  rust = pkgs.rust;
 
-  nixpkgs-mozilla = pkgs.fetchFromGitHub {
-    owner = "mozilla";
-    repo = "nixpkgs-mozilla";
-    # commit from: 2020-2-19
-    rev = "e912ed483e980dfb4666ae0ed17845c4220e5e7c";
-    sha256 = "08fvzb8w80bkkabc1iyhzd15f4sm7ra10jn32kfch5klgl0gj3j3";
-  };
-  moz-pkgs = import <unstable> {
-    overlays = [
-      (import "${nixpkgs-mozilla.out}/rust-overlay.nix")
-    ];
-  };
-
-  # rustPlatfromFromBinary = {version, date, sha256}:
-  #   (rust.makeRustPlatform
-  #   (pkgs.callPackage <unstable/pkgs/development/compilers/rust/binary.nix> rec {
-  #     inherit version;
-
-  #     platform = rust.toRustTarget stdenv.hostPlatform;
-  #     versionType = date;
-
-  #     src = pkgs.fetchurl {
-  #       url = "https://static.rust-lang.org/dist/${date}/rust-${version}-${platform}.tar.gz";
-  #       inherit sha256;
-  #     };
-  #   }));
-  # nightlyRustPlatform = rustPlatfromFromBinary {
-  #   version = "nightly";
-  #   date = "2020-03-12";
-  #   sha256 = "0jhggcwr852c4cqb4qv9a9c6avnjrinjnyzgfi7sx7n1piyaad43";
-  # };
-
-  nightlyRust =
-    let
-      nightly = (moz-pkgs.rustChannelOf {
-        date = "2020-03-12";
-        channel = "nightly";
-      });
-      # include rust-src component
-      # https://github.com/mozilla/nixpkgs-mozilla/issues/51#issuecomment-386079415
-    in nightly.rust.override {
-        extensions = [
-          "rust-std"
-          "rust-src"
-        ];
-      };
-  nightlyRustPlatform = pkgs.makeRustPlatform {
-    rustc = nightlyRust;
-    cargo = nightlyRust;
-  };
-
-  rust-xtensa = (pkgs.callPackage ./rust-xtensa.nix {
-    #rustPlatform = nightlyRustPlatform;
-  });
-  xbuild = pkgs.callPackage ./xbuild.nix {
-    rustPlatform = pkgs.makeRustPlatform rust-xtensa;
-  };
-  xargo = pkgs.callPackage ./xargo.nix {
-    rustPlatform = pkgs.makeRustPlatform rust-xtensa;
-  };
+  rust-esp = pkgs.callPackage ./rust-esp-nix {};
 in
 pkgs.mkShell {
     buildInputs = [ 
-      esp-idf
-      esp32-toolchain
-      xargo
-      xbuild
-      rust-xtensa.rustc
+      rust-esp.xbuild
+      rust-esp.bindgen
+      rust-esp.rustc
+      rust-esp.cargo
+      rust-esp.esp-idf
+      rust-esp.esp32-toolchain
       pkgs.rustfmt
     ];
 
     shellHook = ''
 set -e
 
-export XARGO_RUST_SRC="${rust-xtensa.rust-src}/src"
-export LLVM_XTENSA="${llvm-xtensa}"
-export LIBCLANG_PATH="${llvm-xtensa}/lib"
+${rust-esp.env}
 
-export IDF_PATH=${esp-idf}
-export IDF_TOOLS_PATH=${esp32-toolchain}
-
-export CFLAGS_COMPILE="-Wno-error=incompatible-pointer-types -Wno-error=implicit-function-declaration"
-export OPENOCD_SCRIPTS=$IDF_TOOLS_PATH/tools/openocd-esp32/share/openocd/scripts
-export NIX_CFLAGS_LINK=-lncurses
-export PATH=$PATH:~/.cargo/bin:${esp-idf}/tools:${esp-idf}/components/esptool_py/esptool:$IDF_TOOLS_PATH/tools/esp32ulp-elf/bin:$IDF_TOOLS_PATH/tools/openocd-esp32/bin:$IDF_TOOLS_PATH/tools/xtensa-esp32-elf/bin
+if ! [ -d esp-idf ]; then
+  mkdir -p esp-idf
+  pushd esp-idf
+  git init
+  git remote add origin https://github.com/espressif/esp-idf
+  git fetch --depth 1 origin 0a03a55c1eb44a354c9ad5d91d91da371fe23f84
+  git checkout FETCH_HEAD
+  git submodule update --init --recursive
+  popd
+fi
+IDF42_PATH=${rust-esp.esp-idf};
+IDF_PATH=$(pwd)/esp-idf
     '';
 }
